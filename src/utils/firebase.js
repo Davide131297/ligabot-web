1ab0908fd1b54b28ae808680c0d22bf5faceb879
Davide131297 <chiffi.davide@gmx.de>
2024-06-20 16:16:27 +0200

    Install Firebase Firestore

---
+++ src/utils/firebase.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAZNmqWRkFhGAqAIiQbIlgoA6hlD8BJU-8",
+  authDomain: "ligabot-38d61.firebaseapp.com",
+  databaseURL: "https://ligabot-38d61-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "ligabot-38d61",
+  storageBucket: "ligabot-38d61.appspot.com",
+  messagingSenderId: "99722118463",
+  appId: "1:99722118463:web:7201e994436d2f2bca3f8a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export default {db};
