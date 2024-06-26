6fe5d3ab6b459504b0c49b30bd93d0b2577f6233
davidechiffi <davide.chiffi@iu-study.org>
2024-06-27 01:40:56 +0200

    Home

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,6 +1,10 @@
 import { Stepper } from '@mantine/core';
 import { Text } from '@mantine/core';
+import { Image } from '@mantine/core';
+import { Title } from '@mantine/core';
 
+import BefehlAntworten from '../Components/HomeBilder/BefehlAntworten.png';
+import BotBefehle from '../Components/HomeBilder/BotBefehle.png';
 
 const Home = () => {
 
@@ -12,9 +16,31 @@ const Home = () => {
                     <Stepper.Step label="Step 1" description="Registrieren" />
                     <Stepper.Step label="Step 2" description="Anmelden" />
                     <Stepper.Step label="Step 3" description="Liga erstellen Button drücken" />
+                    <Stepper.Step label="Step 4" description="Bot in deinen Discord Server einladen" />
                     <Stepper.Step label="Step 4" description="Fahrer hinzufügen" />
                 </Stepper>
             </div>
+
+            <div style={{ display: 'flex', gap: '5px' }}> {/* Äußerer Container für horizontale Anordnung */}
+                <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}> {/* Container für den ersten Titel und Bild */}
+                    <Title order={2}>Verfügbare Slash Befehle</Title>
+                    <Image
+                        style={{ width: '300px', height: '300px', objectFit: 'contain' }}
+                        radius="md"
+                        src={BefehlAntworten}
+                    />
+                </div>
+
+                <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}> {/* Container für den zweiten Titel und Bild */}
+                    <Title order={2}>Beispielantworten</Title>
+                    <Image
+                        style={{ width: '300px', height: '300px', objectFit: 'contain' }}
+                        radius="md"
+                        src={BotBefehle}
+                    />
+                </div>
+            </div>
+
         </>
     );
 }
