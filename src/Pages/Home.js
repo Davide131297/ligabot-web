4d0195b81076553e865d9cf9cb6368b0f67b6245
Davide131297 <chiffi.davide@gmx.de>
2024-06-29 11:46:05 +0200

    Home

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,7 +1,4 @@
-import { Stepper } from '@mantine/core';
-import { Text } from '@mantine/core';
-import { Image } from '@mantine/core';
-import { Title } from '@mantine/core';
+import { Stepper, Text, Image, Title, SimpleGrid, Center, Space } from '@mantine/core';
 
 import BefehlAntworten from '../Components/HomeBilder/BefehlAntworten.png';
 import BotBefehle from '../Components/HomeBilder/BotBefehle.png';
@@ -10,36 +7,44 @@ const Home = () => {
 
     return (
         <>
-            <div>
-                <Text size="lg">Das sind die Schritte zum F1 Ligabot</Text>
-                <Stepper orientation="vertical">
-                    <Stepper.Step label="Step 1" description="Registrieren" />
-                    <Stepper.Step label="Step 2" description="Anmelden" />
-                    <Stepper.Step label="Step 3" description="Liga erstellen Button drücken" />
-                    <Stepper.Step label="Step 4" description="Bot in deinen Discord Server einladen" />
-                    <Stepper.Step label="Step 4" description="Fahrer hinzufügen" />
-                </Stepper>
-            </div>
+            <Center>
+                <Title order={1}>Willkommen beim F1 Ligabot</Title>
+            </Center>
 
-            <div style={{ display: 'flex', gap: '5px' }}> {/* Äußerer Container für horizontale Anordnung */}
-                <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}> {/* Container für den ersten Titel und Bild */}
-                    <Title order={2}>Verfügbare Slash Befehle</Title>
+            <Space h="xl" />
+
+            <SimpleGrid cols={3}>
+                <div>
+                    <Title order={2}>Das sind die Schritte zum F1 Ligabot</Title>
+                    <Space h="sm" />
+                    <Stepper orientation="vertical">
+                        <Stepper.Step label="Step 1" description="Registrieren" />
+                        <Stepper.Step label="Step 2" description="Anmelden" />
+                        <Stepper.Step label="Step 3" description="Liga erstellen Button drücken" />
+                        <Stepper.Step label="Step 4" description="Bot in deinen Discord Server einladen" />
+                        <Stepper.Step label="Step 4" description="Fahrer hinzufügen" />
+                        <Stepper.Step label="Step 5" description="Ergebnisse Eintragen" />
+                    </Stepper>
+                </div>
+
+                <div> {/* Container für den ersten Titel und Bild */}
+                    <Title order={2}>Beispielantworten</Title>
+                    <Space h="sm" />
                     <Image
-                        style={{ width: '300px', height: '300px', objectFit: 'contain' }}
                         radius="md"
                         src={BefehlAntworten}
                     />
                 </div>
 
-                <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}> {/* Container für den zweiten Titel und Bild */}
-                    <Title order={2}>Beispielantworten</Title>
+                <div>
+                    <Title order={2}>Verfügbare Slash Befehle</Title>
+                    <Space h="sm" />
                     <Image
-                        style={{ width: '300px', height: '300px', objectFit: 'contain' }}
                         radius="md"
                         src={BotBefehle}
                     />
                 </div>
-            </div>
+            </SimpleGrid>
 
         </>
     );
