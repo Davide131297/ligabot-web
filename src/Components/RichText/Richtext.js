c4465cc7cc039a4d059d6a3bda44ba8bbaaba3bd
davidechiffi <davide.chiffi@iu-study.org>
2024-07-02 01:02:35 +0200

    Bild kann in den Editor geladen werden

--- src/Components/RichText/Richtext.js
+++ src/Components/RichText/Richtext.js
@@ -1,4 +1,5 @@
-import { RichTextEditor, Link } from '@mantine/tiptap';
+import React, { useCallback } from 'react'
+import { RichTextEditor, Link, useRichTextEditorContext } from '@mantine/tiptap';
 import { useEditor } from '@tiptap/react';
 import Highlight from '@tiptap/extension-highlight';
 import StarterKit from '@tiptap/starter-kit';
@@ -9,7 +10,8 @@ import SubScript from '@tiptap/extension-subscript';
 import Placeholder from '@tiptap/extension-placeholder';
 import TextStyle from '@tiptap/extension-text-style';
 import { Color } from '@tiptap/extension-color';
-
+import { Image } from '@tiptap/extension-image';
+import { CiImageOn } from "react-icons/ci";
 
 export default function RichText() {
 
@@ -22,80 +24,126 @@ export default function RichText() {
           SubScript,
           Highlight,
           TextStyle,
-          Color,
+          Color, 
+          Image.configure({
+            inline: true,
+            HTMLAttributes: {
+                alt: 'Ligastartseite',
+                style: 'width: 100px; height: auto;',
+            },
+          }),
           TextAlign.configure({ types: ['heading', 'paragraph'] }),
           Placeholder.configure({ placeholder: 'Hier kannst du die Ligastartseite anpassen.' })
         ],
     });
 
+    const addImage = useCallback(() => {
+    // Erstellen eines 'input' Elements für Dateiauswahl
+        const input = document.createElement('input');
+        input.setAttribute('type', 'file');
+        input.setAttribute('accept', 'image/*'); // Nur Bilder akzeptieren
+        input.click(); // Öffnet den Dateiauswahldialog
+
+        input.onchange = async (e) => {
+            const file = e.target.files[0];
+            if (file) {
+                // Hier könnte man das Bild zum Server hochladen und eine URL zurückbekommen
+                // Für dieses Beispiel lesen wir stattdessen das Bild als Data-URL
+                const reader = new FileReader();
+                reader.onload = (e) => {
+                    const src = e.target.result;
+                    editor.isFocused = true;
+                    editor.chain().focus().setImage({ src }).run();
+                };
+                reader.readAsDataURL(file);
+            }
+        };
+    }, [editor]);
+
+    function showEditor() {
+        console.log(editor.getJSON());
+    }
+
     return (
-        <RichTextEditor editor={editor}>
-            <RichTextEditor.Toolbar sticky stickyOffset={60}>
+        <>
+            <RichTextEditor editor={editor}>
+                <RichTextEditor.Toolbar sticky stickyOffset={60}>
 
-                <RichTextEditor.ControlsGroup>
-                    <RichTextEditor.Undo />
-                    <RichTextEditor.Redo />
-                </RichTextEditor.ControlsGroup>
+                    <RichTextEditor.ControlsGroup>
+                        <RichTextEditor.Undo />
+                        <RichTextEditor.Redo />
+                    </RichTextEditor.ControlsGroup>
 
-                <RichTextEditor.ControlsGroup>
-                    <RichTextEditor.Bold />
-                    <RichTextEditor.Italic />
-                    <RichTextEditor.Underline />
-                    <RichTextEditor.ColorPicker
-                        colors={[
-                            '#25262b',
-                            '#868e96',
-                            '#fa5252',
-                            '#e64980',
-                            '#be4bdb',
-                            '#7950f2',
-                            '#4c6ef5',
-                            '#228be6',
-                            '#15aabf',
-                            '#12b886',
-                            '#40c057',
-                            '#82c91e',
-                            '#fab005',
-                            '#fd7e14',
-                        ]}
-                    />
-                    <RichTextEditor.Strikethrough />
-                    <RichTextEditor.ClearFormatting />
-                    <RichTextEditor.Highlight />
-                    <RichTextEditor.Code />
-                </RichTextEditor.ControlsGroup>
+                    <RichTextEditor.ControlsGroup>
+                        <RichTextEditor.Bold />
+                        <RichTextEditor.Italic />
+                        <RichTextEditor.Underline />
+                        <RichTextEditor.ColorPicker
+                            colors={[
+                                '#25262b',
+                                '#868e96',
+                                '#fa5252',
+                                '#e64980',
+                                '#be4bdb',
+                                '#7950f2',
+                                '#4c6ef5',
+                                '#228be6',
+                                '#15aabf',
+                                '#12b886',
+                                '#40c057',
+                                '#82c91e',
+                                '#fab005',
+                                '#fd7e14',
+                            ]}
+                        />
+                        <RichTextEditor.Strikethrough />
+                        <RichTextEditor.ClearFormatting />
+                        <RichTextEditor.Highlight />
+                        <RichTextEditor.Code />
+                    </RichTextEditor.ControlsGroup>
 
-                <RichTextEditor.ControlsGroup>
-                    <RichTextEditor.H1 />
-                    <RichTextEditor.H2 />
-                    <RichTextEditor.H3 />
-                    <RichTextEditor.H4 />
-                </RichTextEditor.ControlsGroup>
+                    <RichTextEditor.ControlsGroup>
+                        <RichTextEditor.H1 />
+                        <RichTextEditor.H2 />
+                        <RichTextEditor.H3 />
+                        <RichTextEditor.H4 />
+                    </RichTextEditor.ControlsGroup>
 
-                <RichTextEditor.ControlsGroup>
-                    <RichTextEditor.Blockquote />
-                    <RichTextEditor.Hr />
-                    <RichTextEditor.BulletList />
-                    <RichTextEditor.OrderedList />
-                    <RichTextEditor.Subscript />
-                    <RichTextEditor.Superscript />
-                </RichTextEditor.ControlsGroup>
+                    <RichTextEditor.ControlsGroup>
+                        <RichTextEditor.Blockquote />
+                        <RichTextEditor.Hr />
+                        <RichTextEditor.BulletList />
+                        <RichTextEditor.OrderedList />
+                        <RichTextEditor.Subscript />
+                        <RichTextEditor.Superscript />
+                    </RichTextEditor.ControlsGroup>
 
-                <RichTextEditor.ControlsGroup>
-                    <RichTextEditor.Link />
-                    <RichTextEditor.Unlink />
-                </RichTextEditor.ControlsGroup>
+                    <RichTextEditor.ControlsGroup>
+                        <RichTextEditor.Link />
+                        <RichTextEditor.Unlink />
+                    </RichTextEditor.ControlsGroup>
 
-                <RichTextEditor.ControlsGroup>
-                    <RichTextEditor.AlignLeft />
-                    <RichTextEditor.AlignCenter />
-                    <RichTextEditor.AlignJustify />
-                    <RichTextEditor.AlignRight />
-                </RichTextEditor.ControlsGroup>
+                    <RichTextEditor.ControlsGroup>
+                        <RichTextEditor.AlignLeft />
+                        <RichTextEditor.AlignCenter />
+                        <RichTextEditor.AlignJustify />
+                        <RichTextEditor.AlignRight />
+                    </RichTextEditor.ControlsGroup>
 
-            </RichTextEditor.Toolbar>
+                    <RichTextEditor.ControlsGroup>
+                    <RichTextEditor.Control
+                        onClick={() => addImage()}
+                    >
+                        <CiImageOn />
+                        </RichTextEditor.Control>
+                    </RichTextEditor.ControlsGroup>
 
-            <RichTextEditor.Content h={300}/>
-        </RichTextEditor>
+                </RichTextEditor.Toolbar>
+
+                <RichTextEditor.Content h={300} style={{ overflowY: 'scroll' }}/>
+            </RichTextEditor>
+
+            <button onClick={() => showEditor()}>Testen</button>
+        </>
     );
 }
