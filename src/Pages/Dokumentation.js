cd6af0c71b5401717476eec610f2ba7d39a59e52
Davide131297 <chiffi.davide@gmx.de>
2024-07-03 14:25:45 +0200

    Dokumentation

--- src/Pages/Dokumentation.js
+++ src/Pages/Dokumentation.js
@@ -1,4 +1,4 @@
-import { Center, Title } from "@mantine/core";
+import { Center, Title, Text } from "@mantine/core";
 import { useMediaQuery } from '@mantine/hooks';
 
 export default function Dokumentation() {  
@@ -9,37 +9,35 @@ export default function Dokumentation() {
             <Center>
                 <Title order={1}>Dokumentation</Title>
             </Center>
+
             <Title order={2}>Einleitung</Title>
-            <p>Die Webanwendung "F1-Manager" ist eine Anwendung, die es ermöglicht, eine eigene Formel 1 Liga zu erstellen und zu verwalten. 
-                Die Anwendung ist in React.js geschrieben und verwendet Firebase als Datenbank. 
-                Die Anwendung besteht aus einer Startseite
-                und einer Einstellungsseite. Die Startseite zeigt die verschiedenen Ligen an, die in der Anwendung erstellt wurden.
-                Die Einstellungsseite ermöglicht es, eine neue Liga zu erstellen oder eine bestehende Liga zu bearbeiten.
-            </p>
-            <Title order={2}>Startseite</Title>
-            <p>Die Startseite zeigt die verschiedenen Ligen an, die in der Anwendung erstellt wurden. 
-                Es wird der Name der Liga und das Logo der Liga angezeigt.
-            </p>
+                <Text>
+                    Der "F1-LigaBot" ist eine Anwendung, die es ermöglicht, eine eigene Formel 1 Liga zu erstellen, verwalten, und die Ergebnisse der Rennen zu speichern.
+                    Es wird jeder Liga eine eigene Webseite erstellt in der die Tabellen & Statistiken der Liga angezeigt werden.
+                    Außerdem wird ein Discord Bot bereitgestellt, der die Ergebnisse der Rennen in einem Discord Server postet.
+                </Text>
+            <Title order={2}>Registrierung</Title>
+                <Text>
+                    Um die Anwendung zu nutzen, musst du deine Liga registrieren. Dazu musst du dich einmal registrieren. Bei der Registrierung musst du die ServerID 
+                    deines Discord Servers angeben, damit der Bot weiß, welcher Server zu welcher Liga gehört.
+                </Text>
+            <Title order={2}>Discord Bot</Title>
+                <Text>
+                    Der Discord Bot ist ein Bot, der in einem Discord Server hinzugefügt werden kann.
+                </Text>
+                <Text>
+                    Der Bot kann die Fahrertabelle und die Konstrukteurstabelle der Liga in einem Discord Server posten.
+                </Text>
+            <Title order={2}>Liga Webseite</Title>
+                <Text>
+                    Die Liga Webseite zeigt die Eingerichtete Liga an. Um auf die LigaSeite zu gelangen, muss die URL so ausschauen: "https://ligabot-38d61.web.app/(Liganame)".
+                    Auf der Liga Seite werden die Fahrertabelle, die Konstrukteurstabelle und die Statistiken der Liga angezeigt.
+                </Text>
             <Title order={2}>Einstellungsseite</Title>
-            <p>Die Einstellungsseite ermöglicht es, eine neue Liga zu erstellen oder eine bestehende Liga zu bearbeiten.
-                Es können der Name der Liga, das Logo der Liga und die Anzahl der Rennen pro Saison eingestellt werden.
-            </p>
-            <Title order={2}>Liga Seite</Title>
-            <p>Die Liga Seite zeigt die verschiedenen Informationen über die Liga an. 
-                Es werden die Fahrertabelle, die Konstrukteurstabelle und die Statistiken der Liga angezeigt.
-            </p>
-            <Title order={2}>Fahrertabelle</Title>
-            <p>Die Fahrertabelle zeigt die verschiedenen Fahrer der Liga an. 
-                Es werden die Position, der Name, das Team, die Punkte und die Siege der Fahrer angezeigt.
-            </p>
-            <Title order={2}>Konstrukteurstabelle</Title>
-            <p>Die Konstrukteurstabelle zeigt die verschiedenen Konstrukteure der Liga an. 
-                Es werden die Position, der Name, die Punkte und die Siege der Konstrukteure angezeigt.
-            </p>
-            <Title order={2}>Statistiken</Title>
-            <p>Die Statistiken zeigen verschiedene Statistiken der Liga an. 
-                Es werden die Anzahl der Rennen, die Anzahl der Fahrer, die Anzahl der Konstrukteure, die Anzahl der Siege und die Anzahl der Punkte angezeigt.
-            </p>
+                <Text>
+                    Die Einstellungsseite ermöglicht es, eine neue Liga zu erstellen oder eine bestehende Liga zu bearbeiten.
+                    Es können der Name der Liga, das Logo der Liga und die Anzahl der Rennen pro Saison eingestellt werden, sowie deren Ergebnisse eingetragen werden.
+                </Text>
         </div>
     );
 }       
