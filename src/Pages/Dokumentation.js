2211421be2b974bba75999afc90bad3335987d9d
davidechiffi <davide.chiffi@iu-study.org>
2024-07-09 22:36:39 +0200

    Demoseite

--- src/Pages/Dokumentation.js
+++ src/Pages/Dokumentation.js
@@ -1,14 +1,12 @@
 import { Center, Title, Text, SimpleGrid, Image, Box, Space, ScrollArea } from "@mantine/core";
 import { useMediaQuery } from '@mantine/hooks';
-import { Carousel } from '@mantine/carousel';
-import React, { useState } from 'react';
+import React from 'react';
 
 import DemoSeite from '../Components/DemoBilder/DemoStartseite.png';
 import StartseiteEditor from '../Components/DemoBilder/StartseiteEditor.png';
 import DemoFahrertabelle from '../Components/DemoBilder/Fahrertabelle.png';
 import DemoKonstrukteuretabelle from '../Components/DemoBilder/Konstrukteurstabelle.png';
 import DemoStatistiken from '../Components/DemoBilder/StatistikenGrafik.png';
-import Buttons from "../Components/DemoBilder/Buttons.png";
 
 export default function Dokumentation() {  
     const matches = useMediaQuery('(max-width: 768px)'); 
@@ -20,17 +18,21 @@ export default function Dokumentation() {
                 <Title order={1}>Dokumentation</Title>
             </Center>
 
+            <Space h="lg" />
+
             <Title order={2}>Einleitung</Title>
                 <Text>
                     Der "F1-LigaBot" ist eine Anwendung, die es ermöglicht, eine eigene Formel 1 Liga zu erstellen, verwalten, und die Ergebnisse der Rennen zu speichern.
                     Es wird jeder Liga eine eigene Webseite erstellt in der die Tabellen & Statistiken der Liga angezeigt werden.
                     Außerdem wird ein Discord Bot bereitgestellt, der die Ergebnisse der Rennen in einem Discord Server postet.
                 </Text>
+            <Space h="lg" />
             <Title order={2}>Registrierung</Title>
                 <Text>
                     Um die Anwendung zu nutzen, musst du deine Liga registrieren. Dazu musst du dich einmal registrieren. Bei der Registrierung musst du die ServerID 
                     deines Discord Servers angeben, damit der Bot weiß, welcher Server zu welcher Liga gehört.
                 </Text>
+            <Space h="lg" />
             <Title order={2}>Discord Bot</Title>
                 <Text>
                     Der Discord Bot ist ein Bot, der in einem Discord Server hinzugefügt werden kann.
@@ -38,6 +40,7 @@ export default function Dokumentation() {
                 <Text>
                     Der Bot kann die Fahrertabelle und die Konstrukteurstabelle der Liga in einem Discord Server posten.
                 </Text>
+            <Space h="lg" />
             <Title order={2}>Liga Webseite</Title>
                 <Text>
                     Die Liga Webseite zeigt die Eingerichtete Liga an. Um auf die LigaSeite zu gelangen, muss die URL so ausschauen: "https://ligabot-38d61.web.app/(Liganame)".
@@ -45,6 +48,7 @@ export default function Dokumentation() {
                 <Text>
                     Auf der Liga Webseite werden die Fahrertabelle, die Konstrukteurstabelle und die Statistiken der Liga angezeigt.
                 </Text>
+            <Space h="lg" />
             <Title order={2}>Einstellungsseite</Title>
                 <Text>
                     Die Einstellungsseite ermöglicht es, eine neue Liga zu erstellen oder eine bestehende Liga zu bearbeiten.
@@ -61,6 +65,7 @@ export default function Dokumentation() {
                 <Text>
                     Die Tabellen "Fahrerwertung" und "Konstrukteurswertung" können auf der Einstellungsseite über den Button "Downloads" als CSV-Datei heruntergeladen werden.
                 </Text>
+            <Space h="lg" />
             <Title order={2}>CSV Download</Title>
                 <Text>
                     Die Tabellen "Fahrerwertung" und "Konstrukteurswertung" können auf der Einstellungsseite über den Button "Downloads" als CSV-Datei heruntergeladen werden.
@@ -68,99 +73,8 @@ export default function Dokumentation() {
                 <Text>
                     Die CSV-Dateien können in Excel oder Google Tabellen geöffnet werden. Bei Excel muss gegebenfalls das das Komma als Trennzeichen eingestellt werden.
                 </Text>
+            <Space h="lg" />
         </div>
-        {!matches && (
-        <div>
-        <Carousel withIndicators height={500} slideGap="md">
-            <Carousel.Slide>
-            <Box
-                bg="#dbdbdb"
-                p="sm"
-            >
-                <Center>
-                    <Image
-                        src={DemoSeite}
-                        radius="md"
-                        h={200}
-                        w="auto"
-                        fit="contain"
-                        alt="Demo Startseite"
-                    />
-                </Center>
-            </Box>
-            </Carousel.Slide>
-            <Carousel.Slide>
-            <Box
-                bg="#dbdbdb"
-                p="sm"
-            >
-                <Center>
-                    <Image
-                        src={StartseiteEditor}
-                        radius="md"
-                        h={400}
-                        w="auto"
-                        fit="contain"
-                        alt="Demo Startseite Editor"
-                    />
-                </Center>
-            </Box>
-            </Carousel.Slide>
-            <Carousel.Slide>
-            <Box
-                bg="#dbdbdb"
-                p="sm"
-            >
-                <Center>
-                    <Image
-                        src={DemoFahrertabelle}
-                        radius="md"
-                        h={300}
-                        w="auto"
-                        fit="contain"
-                        alt="Demo Fahrertabelle"
-                    />
-                </Center>
-            </Box>
-            </Carousel.Slide>
-            <Carousel.Slide>
-            <Box
-                bg="#dbdbdb"
-                p="sm"
-            >
-                <Center>
-                    <Image
-                        src={DemoKonstrukteuretabelle}
-                        radius="md"
-                        h={230}
-                        w="auto"
-                        fit="contain"
-                        alt="Demo Konstrukteurstabelle"
-                    />
-                </Center>
-            </Box>
-            </Carousel.Slide>
-            <Carousel.Slide>
-            <Box
-                bg="#dbdbdb"
-                p="sm"
-            >
-                <Center>
-                    <Image
-                        src={DemoStatistiken}
-                        radius="md"
-                        h={250}
-                        w="auto"
-                        fit="contain"
-                        alt="Demo Statistiken"
-                    />
-                </Center>
-            </Box>
-            </Carousel.Slide>
-        </Carousel>
-        </div>
-        )}
-        {matches && (
         <div>
             <Center>
             <Title order={3}>DemoStartseite</Title>
@@ -169,7 +83,7 @@ export default function Dokumentation() {
                 <Image
                     src={DemoSeite}
                     radius="md"
-                    h="14vh"
+                    h={matches ? "13vh" : "25vh"}
                     w="auto"
                     fit="contain"
                     alt="Demo Startseite"
@@ -183,7 +97,7 @@ export default function Dokumentation() {
                 <Image
                     src={StartseiteEditor}
                     radius="md"
-                    h="30vh"
+                    h={matches ? "30vh" : "40vh"}
                     w="auto"
                     fit="contain"
                     alt="Demo Startseite Editor"
@@ -197,7 +111,7 @@ export default function Dokumentation() {
                 <Image
                     src={DemoFahrertabelle}
                     radius="md"
-                    h="19vh"
+                    h={matches ? "19vh" : "35vh"}
                     w="auto"
                     fit="contain"
                     alt="Demo Fahrertabelle"
@@ -211,7 +125,7 @@ export default function Dokumentation() {
                 <Image
                     src={DemoKonstrukteuretabelle}
                     radius="md"
-                    h="14vh"
+                    h={matches ? "14vh" : "25vh"}
                     w="auto"
                     fit="contain"
                     alt="Demo Konstrukteurstabelle"
@@ -225,7 +139,7 @@ export default function Dokumentation() {
                 <Image
                     src={DemoStatistiken}
                     radius="md"
-                    h="16vh"
+                    h={matches ? "15vh" : "30vh"}
                     w="auto"
                     fit="contain"
                     alt="Demo Statistiken"
@@ -237,7 +151,6 @@ export default function Dokumentation() {
             </Center>
 
         </div>
-        )}
         </SimpleGrid>
     );
 }       
